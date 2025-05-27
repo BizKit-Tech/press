fbd2ebe093ae3712823b97e8424b3edfbc33d8a1
Gab Barbudo <gabbarbudo@bizkittechnologies.com>
2025-05-27 10:50:09 +0000

    feat: Handle backup restoration in site setup

--- press/press/doctype/site/site_setup.py
+++ press/press/doctype/site/site_setup.py
@@ -56,11 +56,13 @@ class SiteSetup:
             self.disable_bench_watch()
             self.enable_automatic_bench_start()
             self.setup_production()
+            self.download_backup()
             self.create_site()
+            self.restore_files()
             self.install_additional_apps()
             self.set_developer_mode()
             self.restart_bench()
-            self.remove_fail2ban() 
+            self.remove_fail2ban()
             self.run_initial_setup()
         
         self.update_agent_job_status()
@@ -122,7 +124,19 @@ class SiteSetup:
                     "parent": "Site Setup",
                     "parentfield": "steps",
                     "parenttype": "Agent Job Type",
+                    "step_name": "Download Backup"
+                },
+                {
+                    "parent": "Site Setup",
+                    "parentfield": "steps",
+                    "parenttype": "Agent Job Type",
                     "step_name": "Create Site"
+                },
+                {
+                    "parent": "Site Setup",
+                    "parentfield": "steps",
+                    "parenttype": "Agent Job Type",
+                    "step_name": "Restore Files"
                 },
                 {
                     "parent": "Site Setup",
@@ -308,19 +322,98 @@ class SiteSetup:
         output, traceback = self.execute_commands(commands)
         self.update_agent_job_step("Setup Production", start_time, output, traceback)
 
+    def download_backup(self):
+        start_time = now()
+
+        if not self.site.restored_from_backup:
+            self.update_agent_job_step("Download Backup", start_time, skipped=True)
+            return
+
+        commands = [
+            'echo "Downloading backup files..."',
+        ]
+
+        backup_doc = frappe.get_doc("Site Backup", self.site.restored_from_backup)
+        db_backup = backup_doc.database_url
+        config_file_backup = backup_doc.config_file_url
+        public_files_backup = backup_doc.public_url
+        private_files_backup = backup_doc.private_url
+
+        if db_backup:
+            commands.append(f'cd {self.frappe_bench_dir} && wget {db_backup}')
+            self.db_backup_path = db_backup.split("/")[-1]
+
+        if config_file_backup:
+            commands.append(f'cd {self.frappe_bench_dir} && wget {config_file_backup}')
+            config_path = config_file_backup.split("/")[-1]
+        
+        if public_files_backup:
+            commands.append(f'cd {self.frappe_bench_dir} && wget {public_files_backup}')
+            self.public_path = public_files_backup.split("/")[-1]
+
+        if private_files_backup:
+            commands.append(f'cd {self.frappe_bench_dir} && wget {private_files_backup}')
+            self.private_path = private_files_backup.split("/")[-1]
+
+        list_files = f'source ~/.profile && (cd {self.frappe_bench_dir} && ls -lh)'
+        commands.append(list_files)
+        
+        output, traceback = self.execute_commands(commands)
+        self.update_agent_job_step("Download Backup", start_time, output, traceback)
+
     def create_site(self):
         start_time = now()
         admin_password = self.site.get_password("admin_password")
-        commands = [
-            'echo "Creating site..."',
-            f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} new-site --db-name {self.db_name} --mariadb-root-username {self.db_user} --mariadb-root-password {self.db_root_password} --admin-password {admin_password} {self.server_abbr} --force)',
-            f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} --site {self.server_abbr} install-app erpnext --run-patches)',
-            f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} --site {self.server_abbr} install-app bizkit_core --run-patches)',
-            f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} migrate --skip-failing) # Workaround: migrate and skip failing patches before running update to build prereqs first',
-            f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} update --patch --build --no-backup)',
-        ]
+
+        if self.site.restored_from_backup:
+            commands = [
+                'echo "Creating site..."',
+                f'source ~/.profile && (cd {self.frappe_bench_dir} && gunzip -c {self.db_backup_path} > backup.sql)',
+                f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} new-site --db-name {self.db_name} --mariadb-root-username {self.db_user} --mariadb-root-password {self.db_root_password} --admin-password {admin_password} --source_sql {self.frappe_bench_dir}/backup.sql {self.server_abbr} --force)',
+                f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} --site {self.server_abbr} set-admin-password {admin_password})',
+                'echo "Installing apps..."',
+                f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} --site {self.server_abbr} install-app erpnext)',
+                f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} --site {self.server_abbr} install-app bizkit_core)',
+                'echo "Running patches and build..."',
+                f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} update --patch --build --no-backup)'
+            ]
+        else:
+            commands = [
+                'echo "Creating site..."',
+                f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} new-site --db-name {self.db_name} --mariadb-root-username {self.db_user} --mariadb-root-password {self.db_root_password} --admin-password {admin_password} {self.server_abbr} --force)',
+                'echo "Installing apps..."',
+                f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} --site {self.server_abbr} install-app erpnext --run-patches)',
+                f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} --site {self.server_abbr} install-app bizkit_core --run-patches)',
+                'echo "Running patches and build..."',
+                f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} migrate --skip-failing) # Workaround: migrate and skip failing patches before running update to build prereqs first',
+                f'source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} update --patch --build --no-backup)'
+            ]
+
+        if self.environment == "Production":
+            commands.append(f'source ~/.profile && (cd {self.frappe_bench_dir} && sudo {self.bench_path} setup production ubuntu --yes)')
+
         output, traceback = self.execute_commands(commands)
         self.update_agent_job_step("Create Site", start_time, output, traceback)
+
+    def restore_files(self):
+        start_time = now()
+        
+        if not self.site.restored_from_backup:
+            self.update_agent_job_step("Restore Files", start_time, skipped=True)
+            return
+        
+        commands = [
+            'echo "Restoring files..."',
+        ]
+
+        if self.public_path:
+            commands.append(f'''source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} execute frappe.installer.extract_files --args "'{self.server_abbr}','{self.public_path}'")''')
+        
+        if self.private_path:
+            commands.append(f'''source ~/.profile && (cd {self.frappe_bench_dir} && {self.bench_path} execute frappe.installer.extract_files --args "'{self.server_abbr}','{self.private_path}'")''')
+
+        output, traceback = self.execute_commands(commands)
+        self.update_agent_job_step("Restore Files", start_time, output, traceback)
 
     def install_additional_apps(self):
         start_time = now()
@@ -344,7 +437,7 @@ class SiteSetup:
                 commands.append(prereq_script_command)
 
             get_app_command = f"{self.bench_path} get-app {repo_url} --branch {branch}"
-            install_command = f"{self.bench_path} --site {self.server_abbr} install-app {app} --run-patches"
+            install_command = f"{self.bench_path} --site {self.server_abbr} install-app {app}"
 
             formatted_get_app_command = main_command.format(frappe_bench_dir=self.frappe_bench_dir, command=get_app_command)
             formatted_install_command = main_command.format(frappe_bench_dir=self.frappe_bench_dir, command=install_command)
@@ -393,6 +486,7 @@ class SiteSetup:
         
         commands = [
             'echo "Removing fail2ban..."',
+            'source ~/.profile && sudo systemctl stop fail2ban',
             'source ~/.profile && sudo apt-get remove --auto-remove fail2ban --yes',
         ]
         output, traceback = self.execute_commands(commands)
@@ -400,6 +494,11 @@ class SiteSetup:
 
     def run_initial_setup(self):
         start_time = now()
+
+        if self.site.restored_from_backup:
+            self.update_agent_job_step("Run Initial Setup", start_time, skipped=True)
+            return
+
         hris_config = ""
         answer_no = "yes n | "
         if "HRIS" in self.site.product:
