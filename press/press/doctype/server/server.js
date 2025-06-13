7154c0655cdc5d02525aff8ca6b1e5becff45e09
Gab Barbudo <gabbarbudo@bizkittechnologies.com>
2025-06-13 09:18:57 +0000

    feat: Add termination protection action buttons in desk
    * chore: Clean up unused action buttons

--- press/press/doctype/server/server.js
+++ press/press/doctype/server/server.js
@@ -69,159 +69,171 @@ frappe.ui.form.on('Server', {
 				true,
 				frm.doc.status !== 'Archived' && frm.doc.instance_state === 'Running',
 			],
-			[__('Update Agent'), 'update_agent', true, frm.doc.is_server_setup],
 			[
-				__('Update Agent Ansible'),
-				'update_agent_ansible',
+				__('Enable Termination Protection'),
+				'enable_termination_protection',
 				true,
-				frm.doc.is_server_setup,
+				frm.doc.termination_protection === 'Disabled',
 			],
 			[
-				__('Prepare Server'),
-				'prepare_server',
+				__('Disable Termination Protection'),
+				'disable_termination_protection',
 				true,
-				!frm.doc.is_server_prepared,
+				frm.doc.termination_protection === 'Enabled',
 			],
+			// [__('Update Agent'), 'update_agent', true, frm.doc.is_server_setup],
+			// [
+			// 	__('Update Agent Ansible'),
+			// 	'update_agent_ansible',
+			// 	true,
+			// 	frm.doc.is_server_setup,
+			// ],
+			// [
+			// 	__('Prepare Server'),
+			// 	'prepare_server',
+			// 	true,
+			// 	!frm.doc.is_server_prepared,
+			// ],
 			[__('Setup Server'), 'setup_server', true, !frm.doc.is_server_setup],
-			[
-				__('Add to Proxy'),
-				'add_upstream_to_proxy',
-				true,
-				frm.doc.is_server_setup && !frm.doc.is_upstream_setup,
-			],
-			[
-				__('Setup Replication'),
-				'setup_replication',
-				true,
-				frm.doc.is_server_setup &&
-					!frm.doc.is_primary &&
-					!frm.doc.is_replication_setup,
-			],
-			[
-				__('Setup Rename'),
-				'rename_server',
-				true,
-				frm.doc.is_server_setup &&
-					frm.doc.is_server_prepared &&
-					!frm.doc.is_server_renamed,
-			],
-			[
-				__('Fetch Keys'),
-				'fetch_keys',
-				false,
-				frm.doc.is_server_setup &&
-					(!frm.doc.frappe_public_key || !frm.doc.root_public_key),
-			],
-			[__('Update TLS Certificate'), 'update_tls_certificate', true],
-			[
-				__('Auto Scale Workers'),
-				'auto_scale_workers',
-				true,
-				frm.doc.status == 'Active' &&
-					frm.doc.is_primary &&
-					frm.doc.is_server_setup,
-			],
-			[
-				__('Cleanup Unused Files'),
-				'cleanup_unused_files',
-				true,
-				frm.doc.status == 'Active' && frm.doc.is_server_setup,
-			],
-			[__('Create Image'), 'create_image', true, frm.doc.status == 'Active'],
-			[__('Archive'), 'archive', true, frm.doc.status !== 'Archived'],
-			[__('Setup Fail2ban'), 'setup_fail2ban', true, frm.doc.is_server_setup],
-			[
-				__('Setup MySQLdump'),
-				'setup_mysqldump',
-				true,
-				frm.doc.is_server_setup && frm.doc.status == 'Active',
-			],
-			[
-				__('Whitelist Server'),
-				'whitelist_ipaddress',
-				false,
-				frm.doc.is_server_setup,
-			],
-			[
-				__('Agent Setup Proxy IP'),
-				'agent_set_proxy_ip',
-				false,
-				frm.doc.is_server_setup,
-			],
-			[
-				__('Setup Agent Sentry'),
-				'setup_agent_sentry',
-				false,
-				frm.doc.is_server_setup,
-			],
-			[
-				__('Show Agent Password'),
-				'show_agent_password',
-				false,
-				frm.doc.is_server_setup,
-			],
-			[
-				__('Setup Standalone'),
-				'setup_standalone',
-				false,
-				frm.doc.is_server_setup &&
-					frm.doc.is_standalone &&
-					!frm.doc.is_standalone_setup,
-			],
-			[
-				__('Fetch Security Updates'),
-				'fetch_security_updates',
-				false,
-				frm.doc.is_server_setup,
-			],
-			[
-				__('Configure SSH logging'),
-				'configure_ssh_logging',
-				false,
-				frm.doc.is_server_setup,
-			],
-			[
-				__('Reset Usage for all sites'),
-				'reset_sites_usage',
-				true,
-				frm.doc.is_server_setup,
-			],
-			[
-				__('Reboot with serial console'),
-				'reboot_with_serial_console',
-				true,
-				frm.doc.provider === 'AWS EC2',
-			],
-			[
-				__('Enable Public Bench and Site Creation'),
-				'enable_server_for_new_benches_and_site',
-				true,
-				frm.doc.virtual_machine,
-			],
-			[
-				__('Disable Public Bench and Site Creation'),
-				'disable_server_for_new_benches_and_site',
-				true,
-				frm.doc.virtual_machine,
-			],
-			[
-				__('Set Swappiness and SysRq'),
-				'set_swappiness',
-				false,
-				frm.doc.is_server_setup,
-			],
-			[
-				__('Mount Volumes'),
-				'mount_volumes',
-				true,
-				frm.doc.virtual_machine && frm.doc.mounts,
-			],
-			[
-				__('Connect to RDS'),
-				'connect_to_rds',
-				true,
-				frm.doc.database_server && !frm.doc.is_connected_to_database,
-			],
+			// [
+			// 	__('Add to Proxy'),
+			// 	'add_upstream_to_proxy',
+			// 	true,
+			// 	frm.doc.is_server_setup && !frm.doc.is_upstream_setup,
+			// ],
+			// [
+			// 	__('Setup Replication'),
+			// 	'setup_replication',
+			// 	true,
+			// 	frm.doc.is_server_setup &&
+			// 		!frm.doc.is_primary &&
+			// 		!frm.doc.is_replication_setup,
+			// ],
+			// [
+			// 	__('Setup Rename'),
+			// 	'rename_server',
+			// 	true,
+			// 	frm.doc.is_server_setup &&
+			// 		frm.doc.is_server_prepared &&
+			// 		!frm.doc.is_server_renamed,
+			// ],
+			// [
+			// 	__('Fetch Keys'),
+			// 	'fetch_keys',
+			// 	false,
+			// 	frm.doc.is_server_setup &&
+			// 		(!frm.doc.frappe_public_key || !frm.doc.root_public_key),
+			// ],
+			// [__('Update TLS Certificate'), 'update_tls_certificate', true],
+			// [
+			// 	__('Auto Scale Workers'),
+			// 	'auto_scale_workers',
+			// 	true,
+			// 	frm.doc.status == 'Active' &&
+			// 		frm.doc.is_primary &&
+			// 		frm.doc.is_server_setup,
+			// ],
+			// [
+			// 	__('Cleanup Unused Files'),
+			// 	'cleanup_unused_files',
+			// 	true,
+			// 	frm.doc.status == 'Active' && frm.doc.is_server_setup,
+			// ],
+			// [__('Create Image'), 'create_image', true, frm.doc.status == 'Active'],
+			// [__('Archive'), 'archive', true, frm.doc.status !== 'Archived'],
+			// [__('Setup Fail2ban'), 'setup_fail2ban', true, frm.doc.is_server_setup],
+			// [
+			// 	__('Setup MySQLdump'),
+			// 	'setup_mysqldump',
+			// 	true,
+			// 	frm.doc.is_server_setup && frm.doc.status == 'Active',
+			// ],
+			// [
+			// 	__('Whitelist Server'),
+			// 	'whitelist_ipaddress',
+			// 	false,
+			// 	frm.doc.is_server_setup,
+			// ],
+			// [
+			// 	__('Agent Setup Proxy IP'),
+			// 	'agent_set_proxy_ip',
+			// 	false,
+			// 	frm.doc.is_server_setup,
+			// ],
+			// [
+			// 	__('Setup Agent Sentry'),
+			// 	'setup_agent_sentry',
+			// 	false,
+			// 	frm.doc.is_server_setup,
+			// ],
+			// [
+			// 	__('Show Agent Password'),
+			// 	'show_agent_password',
+			// 	false,
+			// 	frm.doc.is_server_setup,
+			// ],
+			// [
+			// 	__('Setup Standalone'),
+			// 	'setup_standalone',
+			// 	false,
+			// 	frm.doc.is_server_setup &&
+			// 		frm.doc.is_standalone &&
+			// 		!frm.doc.is_standalone_setup,
+			// ],
+			// [
+			// 	__('Fetch Security Updates'),
+			// 	'fetch_security_updates',
+			// 	false,
+			// 	frm.doc.is_server_setup,
+			// ],
+			// [
+			// 	__('Configure SSH logging'),
+			// 	'configure_ssh_logging',
+			// 	false,
+			// 	frm.doc.is_server_setup,
+			// ],
+			// [
+			// 	__('Reset Usage for all sites'),
+			// 	'reset_sites_usage',
+			// 	true,
+			// 	frm.doc.is_server_setup,
+			// ],
+			// [
+			// 	__('Reboot with serial console'),
+			// 	'reboot_with_serial_console',
+			// 	true,
+			// 	frm.doc.provider === 'AWS EC2',
+			// ],
+			// [
+			// 	__('Enable Public Bench and Site Creation'),
+			// 	'enable_server_for_new_benches_and_site',
+			// 	true,
+			// 	frm.doc.virtual_machine,
+			// ],
+			// [
+			// 	__('Disable Public Bench and Site Creation'),
+			// 	'disable_server_for_new_benches_and_site',
+			// 	true,
+			// 	frm.doc.virtual_machine,
+			// ],
+			// [
+			// 	__('Set Swappiness and SysRq'),
+			// 	'set_swappiness',
+			// 	false,
+			// 	frm.doc.is_server_setup,
+			// ],
+			// [
+			// 	__('Mount Volumes'),
+			// 	'mount_volumes',
+			// 	true,
+			// 	frm.doc.virtual_machine && frm.doc.mounts,
+			// ],
+			// [
+			// 	__('Connect to RDS'),
+			// 	'connect_to_rds',
+			// 	true,
+			// 	frm.doc.database_server && !frm.doc.is_connected_to_database,
+			// ],
 		].forEach(([label, method, confirm, condition]) => {
 			if (typeof condition === 'undefined' || condition) {
 				frm.add_custom_button(
@@ -255,32 +267,32 @@ frappe.ui.form.on('Server', {
 		});
 
 		if (frm.doc.is_server_setup) {
-			frm.add_custom_button(
-				__('Increase Swap'),
-				() => {
-					const dialog = new frappe.ui.Dialog({
-						title: __('Increase Swap'),
-						fields: [
-							{
-								fieldtype: 'Int',
-								label: __('Swap Size'),
-								description: __('Size in GB'),
-								fieldname: 'swap_size',
-								default: 4,
-							},
-						],
-					});
+			// frm.add_custom_button(
+			// 	__('Increase Swap'),
+			// 	() => {
+			// 		const dialog = new frappe.ui.Dialog({
+			// 			title: __('Increase Swap'),
+			// 			fields: [
+			// 				{
+			// 					fieldtype: 'Int',
+			// 					label: __('Swap Size'),
+			// 					description: __('Size in GB'),
+			// 					fieldname: 'swap_size',
+			// 					default: 4,
+			// 				},
+			// 			],
+			// 		});
 
-					dialog.set_primary_action(__('Increase Swap'), (args) => {
-						frm.call('increase_swap', args).then(() => {
-							dialog.hide();
-							frm.refresh();
-						});
-					});
-					dialog.show();
-				},
-				__('Actions'),
-			);
+			// 		dialog.set_primary_action(__('Increase Swap'), (args) => {
+			// 			frm.call('increase_swap', args).then(() => {
+			// 				dialog.hide();
+			// 				frm.refresh();
+			// 			});
+			// 		});
+			// 		dialog.show();
+			// 	},
+			// 	__('Actions'),
+			// );
 		}
 	},
 
